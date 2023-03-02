4a8a65958c7c1dcb30d3f8ccd0b26d583f546aee
ofemiashiru <ofemiashiru@gmail.com>
2023-03-02 23:58:01 +0000

    manipulating the dom

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Hello World');
+
+let mainNav = document.querySelector('ul');
+
+let newLi = document.createElement('li');
+
+let newLink = document.createElement('a')
+
+newLink.innerText = '🌴 Holiday';
+
+newLi.appendChild(newLink);
+
+mainNav.append(newLi)
