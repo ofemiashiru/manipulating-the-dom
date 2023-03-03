29e0c2d9509deb9930c35cf1a07b03d1c53f2777
ofemiashiru <ofemiashiru@gmail.com>
2023-03-03 00:03:14 +0000

    manipulate the dom

--- js/app.js
+++ js/app.js
@@ -11,3 +11,15 @@ newLink.innerText = '🌴 Holiday';
 newLi.appendChild(newLink);
 
 mainNav.append(newLi)
+
+let mainHeader = document.querySelector('h1')
+
+mainHeader.style.fontSize = '20rem'
+mainHeader.style.color = 'blue'
+mainHeader.style.textDecoration = 'underline'
+
+let secondHeader = document.createElement('h2')
+
+secondHeader.innerText = 'Everything you need to know about the DOM'
+
+mainHeader.after(secondHeader) // after method places items after the node
