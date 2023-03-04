9fb753188fc21df3e4ed27ba1a1283b494e4d571
ofemiashiru <ofemiashiru@gmail.com>
2023-03-04 16:12:46 +0000

    using this on buttons - tapping into this properties

--- this.js
+++ this.js
@@ -1,7 +1,19 @@
 $(document).ready(()=>{
 
-    $('p').click(function(){
-        $(this).slideToggle('slow');
+    $('p').click(function(){ // 'p' refers to all paragraphs
+        $(this).slideToggle('slow'); // this keyword refers to the individual p element clicked
+    })
+
+    $('button').on({
+        mouseenter: function(){
+            $(this).removeClass('make-red').addClass('make-blue')
+        },
+        mouseleave: function(){
+            $(this).removeClass('make-blue').addClass('make-red')
+        },
+        click: function(){
+            $(this.previousElementSibling).slideToggle('slow')// using this and tapping into a property 
+        }
     })
 
 })
