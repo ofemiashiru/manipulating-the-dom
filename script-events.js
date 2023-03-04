e03f92147bef505e6c882cc70391d6aede11a0cb
ofemiashiru <ofemiashiru@gmail.com>
2023-03-04 12:54:02 +0000

    jquery events

---
+++ script-events.js
@@ -0,0 +1,21 @@
+$(document).ready(()=>{
+
+    $("#stream1_btn").on("mouseover", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("mouseover", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("mouseover", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+})
