6966484fb81a57ef2ccde55fe37cadcd6860becf
ofemiashiru <ofemiashiru@gmail.com>
2023-03-04 14:08:03 +0000

    jQuery Effects

--- script-events.js
+++ script-events.js
@@ -6,16 +6,44 @@ $(document).ready(()=>{
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
    });
+
    $("#stream2_btn").on("mouseover", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
    });
+
    $("#stream3_btn").on("mouseover", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
    });
+
+
+   $('.btn1').on({
+        mouseenter: function(){
+            $(this).css('color', 'red')
+        },
+        mouseleave: function(){
+            $(this).css('color', 'white')
+        },
+        click: function(){
+            $('img').hide('fade',1000)
+        }
+
+   });
+
+   $('.btn2').on({
+        click: function(){
+            $('img').toggle(1000)
+        }
+   })
+
+   $('.btn3').on({
+        click: function(){
+            $('img').toggle('blind', 1000)
+        }
+   })
 })
